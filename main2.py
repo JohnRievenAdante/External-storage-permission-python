c37a50b8afa9fda05bff18a35a45581db06476ad
JohnRievenAdante <99161575+JohnRievenAdante@users.noreply.github.com>
2023-02-09 22:22:24 +0800

    /

--- main2.py
+++ main2.py
@@ -114,8 +114,7 @@ class Example(MDApp):
     def _show_validation_dialog(self):
         if platform == "android":
             from android.permissions import request_permissions, Permission
-    request_permissions([Permission.CAMERA, Permission.WRITE_EXTERNAL_STORAGE, 
-Permission.READ_EXTERNAL_STORAGE,Permission.READ_MEDIA_IMAGES])
+            request_permissions([Permission.CAMERA, Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE,Permission.READ_MEDIA_IMAGES])
             Environment = autoclass("android.os.Environment")
             logging.info("show validation dialog ok")
             if not Environment:
